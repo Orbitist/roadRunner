0e583ad9163dd9f1c30f613da13b3c833dbd36c5
Nicholas Gunner <gunnernr@gmail.com>
2016-09-10 10:33:36 -0400

    map now updates with location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,16 +17,61 @@ angular.module('starter.controllers', [])
 .controller('RunPlayCtrl', function($scope, $stateParams, $cordovaGeolocation, $ionicPlatform, Runs) {
   $scope.run = Runs.get($stateParams.runId);
 
+  function updateMap(newCoords) {
+    // Update map
+    var newLatLng = new google.maps.LatLng(newCoords.latitude, newCoords.longitude);
+    $scope.marker.setPosition(newLatLng);
+    // Check for actions
+    // Wait 1 second then run getCoords again
+    setTimeout(function (){
+      getCoords();
+    }, 1000);
+    // getCoords()
+  }
+
+  function getCoords() {
+    // Watch user location
+    var posOptions = {timeout: 30000, enableHighAccuracy: true};
+    $cordovaGeolocation.getCurrentPosition(posOptions)
+    .then(function (position) {
+      $scope.coords = position.coords;
+      updateMap(position.coords);
+    }, function(err) {
+      console.log('getCurrentPosition error: ' + angular.toJson(err));
+    });
+  };
+
   var map;
   function showMap(coords) {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: coords.latitude, lng: coords.longitude},
-      zoom: 13
+      zoom: 19,
+      disableDefaultUI: true,
+      mapTypeId: 'hybrid'
     });
+    var marker = new google.maps.Marker({
+      position: {lat: coords.latitude, lng: coords.longitude},
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 5,
+        fillColor: 'yellow',
+        fillOpacity: 0.8,
+        strokeColor: 'gold',
+        strokeWeight: 5
+      },
+      map: map
+    });
+    $scope.marker = marker;
+    // Hide spinner and remove it
+    document.getElementById("spinner").style.opacity = "0";
+    setTimeout(function () {
+      document.getElementById("spinner").style.display = "none";
+    }, 500);
+    getCoords();
   }
 
   $ionicPlatform.ready(function() {
-    var posOptions = {timeout: 10000, enableHighAccuracy: true};
+    var posOptions = {timeout: 30000, enableHighAccuracy: true};
     $cordovaGeolocation.getCurrentPosition(posOptions)
       .then(function (position) {
         $scope.coords = position.coords;
